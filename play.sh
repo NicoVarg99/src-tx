c7f3a44535f26a70777be0c2c2a8beb5a9454e0d
Nicola Salsotto <nicolasalsotto@gmail.com>
2018-04-08 16:59:39 +0200

    Add play script

--- play.sh
+++ play.sh
@@ -1,1 +1,38 @@
 #!/bin/bash
+
+#Generate audio
+./src-tx
+
+echo "Waiting for second '50' to start playing..."
+
+#Sleep until next 50th second
+#sleep $(( $((60 + $((60 - $(date +%S) - 10)) )) % 60 ))
+
+#Play audio
+aplay ./src.wav -q &
+
+sleep 2
+
+echo "SIG1"
+sleep 1
+
+echo "SIG2"
+sleep 1
+
+echo -n "==="
+sleep 1
+
+echo -n "==="
+sleep 1
+
+echo -n "==="
+sleep 1
+
+echo -n "==="
+sleep 1
+
+echo -n "==="
+sleep 2
+
+echo -ne "\n* SYNC *   "
+date
